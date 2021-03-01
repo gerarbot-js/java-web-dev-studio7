83e4c9b83dc5146fbe6c0072f0dcfd12773a4e08
Gerardo Delgado <gdelgado100@gmail.com>
2021-03-01 16:40:56 -0600

    Chapter 8 Studio: Spinning the Discs completed sans Bonus Missions

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -5,6 +5,14 @@ public class Main {
     public static void main(String[] args){
 
         // TODO: Declare and initialize a CD and a DVD object.
+        CD myCD = new CD();
+        DVD myDVD = new DVD("The Thing");
+
+        myCD.spinDisc();
+        myDVD.spinDisc();
+
+        myCD.writeData("Bonus material");
+        System.out.println(myCD.readData());
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
 
